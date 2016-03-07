0a5c028e3618b216537714ee32033a18046cca44
henrimmo <hepsuu@gmail.com>
2016-03-07 21:17:56 +0200

    7. palautus

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by username: params[:username]
-
-    if user && user.authenticate(params[:password]) && !user.iced
-      session[:user_id] = user.id
-      redirect_to user_path(user), notice: "Welcome back!"
-    elsif user.iced
-      redirect_to :back, notice: 'Your account is frozen, please contact admin'
+    if user && user.authenticate(params[:password])
+      if user.locked?
+        redirect_to :back, notice: "Your account is frozen, please contact admin"
+      else
+        session[:user_id] = user.id
+        redirect_to user_path(user), notice: "Welcome back!"
+      end
     else
       redirect_to :back, notice: "Username and/or password mismatch"
     end
@@ -18,24 +19,5 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to :root
-  end
-
-  def create_oauth
-    user = User.find_by username: env["omniauth.auth"].info.nickname
-
-    if user.nil?
-      user = User.create
-      user.username = env["omniauth.auth"].info.nickname
-      user.password = user.password_confirmation = SecureRandom.urlsafe_base64(n=6)
-      user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user), notice: "Welcome!"
-    elsif user && !user.iced
-      session[:user_id] = user.id
-      redirect_to user_path(user), notice: "Welcome back!"
-    elsif user.iced
-      redirect_to :back, notice: 'Your account is frozen, please contact admin'
-    end
-
   end
 end
